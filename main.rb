52c2638e038fad5e2f0f9a4edf6b41da5ca1099c
sunflat <git.sunflat@sunflat.net>
2019-01-23 17:08:05 +0900

    複数ページに対応

--- main.rb
+++ main.rb
@@ -7,8 +7,15 @@ current_date = Date.today - distance
 query = current_date.strftime('日報/%Y/%m/%d')
 
 client = Esa::Client.new(access_token: ENV['ESA_API_TOKEN'], current_team: ENV['TEAM'])
-response = client.posts(q: "in:#{query}")
-puts "#{response.body['posts'].size}件の日報がありました"
-response.body['posts'].each do |p|
-  system %!open "#{p['url']}"!
+page = 1
+count = 0
+loop do
+  response = client.posts(q: "in:#{query}", page: page)
+  count += response.body['posts'].size
+  response.body['posts'].each do |p|
+    system %!open "#{p['url']}"!
+  end
+  page = response.body['next_page']
+  break unless page
 end
+puts "#{count}件の日報がありました"
